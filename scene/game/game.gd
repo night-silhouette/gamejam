fc507b694be07f2b9feb686bb811e2c877c7ab36
night <631434665@qq.com>
2025-11-27 09:54:29 +0800

    代码框架优化,网络同步完善（两端不同发牌）

--- scene/game/game.gd
+++ scene/game/game.gd
@@ -3,13 +3,19 @@ extends Node2D
 @onready var hard_container: Control = $hard_container
 @export var parent_card_list:Array[Resource]
 const CARD_ON_HARD = preload("res://scene/card_on_hard/Card_on_hard.tscn")
+func create_card(src:Resource)->Control:
+	var card = CARD_ON_HARD.instantiate()
+	card.card_source=src
+	return card
 
 var rng = RandomNumberGenerator.new()
 var current_seed:int
 
 var id
 
-@export var player_card={}
+@export var deal_card:Array[Array]:
+	set(value):
+		deal_card=value
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -22,35 +28,64 @@ func _process(delta: float) -> void:
 		animation_player.animation_finished.connect(func(_t):GameX.finished.emit("gamey"))
 		
 		
+var is_first_ready=true
 
-func _ready() -> void:
-	animation_player.play("enter")
-	id = multiplayer.get_unique_id()
-	player_card[id]=[]
+
+@onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
+func frist_ready():
+	if is_first_ready:
+		
+		if multiplayer.is_server():
+			deal_card=GameStateMachine.card_in_hard_index
+		#根据deal_card和是否为服务器发牌
+		if multiplayer.is_server():
+			for i in range(11):
+				var src:Resource=parent_card_list[deal_card[0][i]]
+				GameStateMachine.card_in_hard.push_back(create_card(src))
+		else:
+			for i in range(11):
+				var src:Resource=parent_card_list[deal_card[1][i]]
+				GameStateMachine.card_in_hard.push_back(create_card(src))
+	is_first_ready=false
 	
-	
-	
-	
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+func init_seed():
 	rng.randomize()
 	current_seed=rng.seed	
 	seed(current_seed)
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)#隐藏鼠标
+	init_seed()#初始化随机数
+	
+	
+	id = multiplayer.get_unique_id()
+	if !multiplayer.is_server():#分端初始化
+		multiplayer_synchronizer.synchronized.connect(func():
+			frist_ready()
+			for card in GameStateMachine.card_in_hard:
+				hard_container.add_child(card)	
+			hard_container.container_init()
+			,CONNECT_ONE_SHOT)
+	else:
+		frist_ready()
+		for card in GameStateMachine.card_in_hard:
+			hard_container.add_child(card)	
+		hard_container.container_init()
+		
+	
+	animation_player.play("enter")
+	
+	
+
 	
 	
 	
 	
-	parent_card_list.shuffle()
-	for i in range(11):
-		var src=parent_card_list[i]
-		var card=CARD_ON_HARD.instantiate()
-		card.card_source=src
-		player_card[id].push_back(card)
-		hard_container.add_child(card)
-	hard_container.container_init()
+
+
+		
+
 	
 	
-	
-	
-	
-	
-	
+
+		
+		
--- scene/gane_statemachine/game_state_machine.gd
+++ scene/gane_statemachine/game_state_machine.gd
@@ -1,7 +1,26 @@
 extends State_machine
+var card_in_hard_index:Array[Array]=[[],[]]
+var card_in_hard:Array[Control]=[]
+
+		
+func randi_deal_card():
+	var temp:Array[int]=[]
+	for i in range(23):
+		temp.push_back(i)
+	temp.shuffle()
+	for i in range(11):
+		card_in_hard_index[0].push_back(temp[i])
+	for i in range(12,23):
+		card_in_hard_index[1].push_back(temp[i])
 
 
 func _ready() -> void:
+	
+	
+	if multiplayer.is_server():
+		randi_deal_card()
+	
+	
 	state_map["gamex"]=GameX
 	state_map["loading"]=Loading
 	state_map["main_menu"]=MainMenu
