bd3f964ab1907c61457435e73a58090ca214b9b2
night <631434665@qq.com>
2025-12-24 13:57:11 +0800

    godot_servers_first_test

---
+++ servers/servers.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var peer = WebSocketMultiplayerPeer.new()
+var port = 443
+
+func _ready() -> void:
+	var err=peer.create_server(port)
+	if err!=OK:
+		print("服务器启动失败")
+	else:
+		print("服务器启动success")
+		
+		multiplayer.multiplayer_peer=peer
+		multiplayer.peer_connected.connect(_on_peer_connected)
+		multiplayer.peer_disconnected.connect(_on_peer_disconnected)		
+		
+func _process(delta: float) -> void:
+	if peer.get_connection_status() !=MultiplayerPeer.CONNECTION_DISCONNECTED:
+		peer.poll()
+		
+		
+		
+func _on_peer_connected(id):
+	print(str(id))
+func _on_peer_disconnected(id):
+	pass
+	
